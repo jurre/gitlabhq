bc5bfead20f66b09931f934a8613579810822e0f
Jurre Stender <jurrestender@hotmail.com>
2013-05-21 21:57:48 +0200

    Add hip_chat service hook

---
+++ spec/models/hip_chat_service_spec.rb
@@ -0,0 +1,38 @@
+# == Schema Information
+#
+# Table name: services
+#
+#  id          :integer          not null, primary key
+#  type        :string(255)
+#  title       :string(255)
+#  token       :string(255)
+#  project_id  :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  active      :boolean          default(FALSE), not null
+#  project_url :string(255)
+#
+
+require 'spec_helper'
+
+describe HipChatService do
+  describe "Associations" do
+    it { should belong_to :project }
+    it { should have_one :service_hook }
+  end
+
+  describe "Mass assignment" do
+    it { should_not allow_mass_assignment_of(:project_id) }
+  end
+
+  describe 'commits methods' do
+    before do
+      @service = GitlabCiService.new
+      @service.stub(
+        service_hook: true,
+        project_url: 'RoomName',
+        token: 'verySecret'
+      )
+    end
+  end
+end
